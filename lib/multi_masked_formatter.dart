0849ba818736caf00f1884ced520e3182cf1c938
Alien Coder <59464137+sehoon787@users.noreply.github.com>
2022-11-14 15:15:58 +0900

    Update multi_masked_formatter.dart

--- lib/multi_masked_formatter.dart
+++ lib/multi_masked_formatter.dart
@@ -1,15 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class MultiMaskedTextInputFormatter extends TextInputFormatter {
-  List<String> _masks;
-  String _separator;
-  String _prevMask;
+  late List<String> _masks;
+  String? _separator;
+  late String _prevMask;
 
   MultiMaskedTextInputFormatter(
-      {@required List<String> masks, @required String separator}) {
-    _separator = (separator != null && separator.isNotEmpty) ? separator : null;
-    if (masks != null && masks.isNotEmpty) {
+      {
+        required List<String> masks,
+        String? separator
+      }) {
+
+    _separator = separator ?? '';
+    if (masks.isNotEmpty) {
       _masks = masks;
       _masks.sort((l, r) => l.length.compareTo(r.length));
       _prevMask = masks[0];
@@ -22,35 +25,28 @@ class MultiMaskedTextInputFormatter extends TextInputFormatter {
     final newText = newValue.text;
     final oldText = oldValue.text;
 
-    if (newText.length == 0 ||
-        newText.length < oldText.length ||
-        _masks == null ||
-        _separator == null) {
+    if (newText.isEmpty || newText.length < oldText.length || _separator == null) {
       return newValue;
     }
 
     final pasted = (newText.length - oldText.length).abs() > 1;
     final mask = _masks.firstWhere((value) {
-      final maskValue = pasted ? value.replaceAll(_separator, '') : value;
+      final maskValue = pasted ? value.replaceAll(_separator!, '') : value;
       return newText.length <= maskValue.length;
-    }, orElse: () => null);
-
-    if (mask == null) {
-      return oldValue;
-    }
+    }, orElse: () => '');
 
     final needReset =
-        (_prevMask != mask || newText.length - oldText.length > 1);
+    (_prevMask != mask || newText.length - oldText.length > 1);
     _prevMask = mask;
 
     if (needReset) {
-      final text = newText.replaceAll(_separator, '');
+      final text = newText.replaceAll(_separator!, '');
       String resetValue = '';
       int sep = 0;
 
       for (int i = 0; i < text.length; i++) {
         if (mask[i + sep] == _separator) {
-          resetValue += _separator;
+          resetValue += _separator!;
           ++sep;
         }
         resetValue += text[i];
